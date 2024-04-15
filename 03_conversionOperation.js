60817a63f12d0f6810488e1688e380e559683e29
Rajiv Gupta <rajiv.gupta1.contractor@pepsico.com>
2024-04-15 13:47:49 +0530

    Updated Javascript conversions and operations

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -2,12 +2,12 @@
 
 let score  = 45
 
-// console.log(typeof score);
-// console.log(typeof(score));
+console.log(typeof score);
+console.log(typeof(score));
 
 let valueInNumber = Number(score)
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
 
 // "33" -> convert easily into number 33.
 // "33abc" -> it return NaN(Not a Number)
@@ -16,8 +16,8 @@ let valueInNumber = Number(score)
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-// console.log(typeof isLoggedIn);
-// console.log(booleanIsLoggedIn);
+console.log(typeof isLoggedIn);
+console.log(booleanIsLoggedIn);
 
 // 1 -> true; 0 -> false;
 // "" -> false
@@ -26,31 +26,31 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 69
 
 let stringNumber = String(someNumber)
-// console.log(stringNumber);
-// console.log(typeof stringNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
 
 // ************************* Operations *************************
 
 let value = 3
 let negValue = -value
-// console.log(negValue);
+console.log(negValue);
 
-// console.log(6+9);
-// console.log(2**2);
-// console.log(2%3);
+console.log(6+9);
+console.log(2**2);
+console.log(2%3);
 
 let str1 = "Hello"
 let str2 = " Rajiv"
 let str3 = str1 + str2
-// console.log(str3);
+console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
 
-// console.log(+true);
-// console.log(+"");
+console.log(+true);
+console.log(+"");
 
 let num1, num2, num3
 num1 = num2 = num3 = 2 + 2
@@ -59,3 +59,6 @@ let gameCounter = 100
 gameCounter++;
 ++gameCounter
 console.log(gameCounter);
+
+// link to study in details:
+// https://tc39.es/ecma262/multipage/abstract-operations
