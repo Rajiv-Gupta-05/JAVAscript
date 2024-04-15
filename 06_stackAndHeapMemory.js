3d84430df1a3057743411d9dc7082318d203de9f
Rajiv Gupta <rajiv.gupta1.contractor@pepsico.com>
2024-04-15 16:51:31 +0530

    stack and heap memory in javascript

---
+++ 06_stackAndHeapMemory.js
@@ -0,0 +1,29 @@
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// How memory works in javascript?:
+
+/* Two types of Memory:- Stack and heap 
+All the Primitive datatypes uses: Stack Memory
+All the Non-Primitive datatpes uses: Heap memory */
+
+/* In JavaScript, when variable is declared into the stack memory, it always generates a copy for the same. 
+When object is stored into the heap memory, it always generates a "reference" or original value. */
+
+let myName = "rajivgupta"
+let anotherName = myName
+anotherName = "guptarajiv"
+
+console.log(myName);
+console.log(anotherName);
+
+
+let user1 = {
+    email: "user@gmail.com",
+    upi: "user@ybl"
+}
+
+let user2 = user1
+user2.email = "rajiv@gmail.com"
+
+console.log(user1.email);
+console.log(user2.email);
