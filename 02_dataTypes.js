50c565bea65b442f4bd5ceb36ac2ceed216b9730
Rajiv Gupta <rajiv.gupta1.contractor@pepsico.com>
2024-04-15 12:11:28 +0530

    Javascript datatypes

---
+++ 02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict" // treat all the JS code as newer version.
+
+// alert (3+3) -> Error print because we are using nodejs, not browser. 
+
+let name = "Rajiv"
+let age = 25
+let isLoggedIn = false
+
+// number => 2 ^ 53
+// bigint -> used for big numbers like in trading, stock, big websites.
+// string -> use '' or  ""
+// boolean => true / false
+// null => standalone value and it is an object.
+// undefined => value is unassigned in variable. Null and Undefined both are different things.
+// symbol => when we want to define unique.
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
